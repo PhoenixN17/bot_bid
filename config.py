27673339c561e1050fb7d40c16669f9905bad616
gamescasino111 <nodickparty@gmail.com>
2021-11-05 09:30:49 +0200

    admin

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = '1255381193:AAHBwnVSn_13se_SgI-4kFetJd18SKAziwg' # Токен бота
 db_url = "postgres://xatglgjtjitzgj:d0229affcbea8feeaa6c3251e826e80bc40758905995dbbebe1583147de155ce@ec2-18-213-176-229.compute-1.amazonaws.com:5432/d1uppvrcktqte8"
 HEROKU_LINK = "https://drawbot14.herokuapp.com/"
-admin = [767101864, 231125267] # id админов вписывать сюда
-mod = [767101864, 231125267]
+admin = [767101864, 231125267, 138405449] # id админов вписывать сюда
+mod = [767101864, 231125267, 138405449]
