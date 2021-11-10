efcb84953c4015c97daa13e0ed24954d41931f0e
gamescasino111 <nodickparty@gmail.com>
2021-11-10 11:58:14 +0200

    f

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-TOKEN = '1255381193:AAHBwnVSn_13se_SgI-4kFetJd18SKAziwg' # Токен бота
+TOKEN = '2112234680:AAF9VKlSmTpqYye5dl0baxMY7-zdZ-vW2MM' # Токен бота
 db_url = "postgres://lmamnxpfpfifsw:95a897c3293007ffef330395d8265439dddd857ca8ad134fad0815b502946f12@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/dbutbqc6n5okr9"
 HEROKU_LINK = "https://tgbotkk.herokuapp.com/"
 admin = [767101864, 231125267, 138405449] # id админов вписывать сюда
-mod = [767101864, 231125267, 138405449]
+mod = [767101864, 231125267, 138405449] # Список модераторов, могут менять количество фкоинов
+group_id = "" # id группы для логов
