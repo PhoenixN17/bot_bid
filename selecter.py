bd6be051c88608c476300bbbb29717ad366c1a53
gamescasino111 <nodickparty@gmail.com>
2021-11-11 12:22:38 +0200

    f

--- selecter.py
+++ selecter.py
@@ -1,6 +1,9 @@
 import models
+from models import db
 
-for i in models.ActiveLot.query.all():
-	print(i)
+for i in models.BotUser.query.all():
+        db.session.delete(i)
+
+db.session.commit()
 
 
