b9a39d442fb70cb21d2a0ed5603d7363f25a0d45
Dump-Line <65134933+Dump-Line@users.noreply.github.com>
2021-11-13 11:33:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = '2112234680:AAF9VKlSmTpqYye5dl0baxMY7-zdZ-vW2MM' # Токен бота
-db_url = "postgres://lmamnxpfpfifsw:95a897c3293007ffef330395d8265439dddd857ca8ad134fad0815b502946f12@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/dbutbqc6n5okr9"
-HEROKU_LINK = "https://tgbotkk.herokuapp.com/"
+TOKEN = '1255381193:AAHBwnVSn_13se_SgI-4kFetJd18SKAziwg' # Токен бота
+db_url = "postgres://xatglgjtjitzgj:d0229affcbea8feeaa6c3251e826e80bc40758905995dbbebe1583147de155ce@ec2-18-213-176-229.compute-1.amazonaws.com:5432/d1uppvrcktqte8"
+HEROKU_LINK = "https://drawbot14.herokuapp.com/"
 admin = [767101864, 231125267, 138405449] # id админов вписывать сюда
 mod = [767101864, 231125267, 138405449, 692625483, 899432913, 863389047, 656570771, 502996272, 456162787, 1409532653, 1011984925, 1831435557, 1253351850] # Список модераторов, могут менять количество фкоинов
 group_id = "" # id группы для логов
