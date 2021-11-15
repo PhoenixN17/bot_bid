da1fbbc031a6b89014f482ec4522c32be474cadb
gamescasino111 <nodickparty@gmail.com>
2021-11-15 07:41:35 +0200

    f

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = '2112234680:AAF9VKlSmTpqYye5dl0baxMY7-zdZ-vW2MM' # Токен бота
+TOKEN = '1255381193:AAHBwnVSn_13se_SgI-4kFetJd18SKAziwg' # Токен бота
 db_url = "postgres://lmamnxpfpfifsw:95a897c3293007ffef330395d8265439dddd857ca8ad134fad0815b502946f12@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/dbutbqc6n5okr9"
-HEROKU_LINK = "https://tgbotkk.herokuapp.com/"
+HEROKU_LINK = "https://drawbot14.herokuapp.com/"
 admin = [767101864, 231125267, 138405449] # id админов вписывать сюда
 mod = [767101864, 231125267, 138405449, 692625483, 899432913, 863389047, 656570771, 502996272, 456162787, 1409532653, 1011984925, 1831435557, 1253351850] # Список модераторов, могут менять количество фкоинов
 auc_group_id = "-1001769657247"
