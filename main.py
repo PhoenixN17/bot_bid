4d886b3d71d86ea60bc202320060c091a37ba29f
gamescasino111 <nodickparty@gmail.com>
2021-11-15 10:44:44 +0200

    f

--- main.py
+++ main.py
@@ -18,24 +18,29 @@ from flask import request
 global quiz_status
 quiz_status = True
 
-'''
-user = models.BotUser.query.filter_by(id=223).first()
-print(user)
-#count = 0
-#lot = [i.id for i in models.Auc.query.all()]
-#lots = [x.lot_id for x in models.CompleteQuiz.query.filter_by(user_id=user.user_id).all()]
-#print(len(lot), len(lots))
 
 
+users = models.BotUser.query.all()
+#sfor i in users:
+#	if i.coins == 10:
+#		i.coins = 110
 
-#if i.coins < count + 10:
-	#i.coins = count + 10
-#print(user.coins, count)
 #db.session.commit()
 
+#for i in users:
+#	print(i)
+#	count = 0
+#	for x in models.CompleteQuiz.query.filter_by(user_id=i.user_id).all():
+#		if x.status == "win":
+#			count += int(x.cost)
+#
+#	if i.coins < count + 10:
+#		i.coins = count + 10
+#
+#	db.session.commit()
 
 
-'''
+
 
 # ------ Викторина ------ #
 # выдача
@@ -335,7 +340,522 @@ def top_lader(message):
 			count += 1
 		bot.send_message(config.balance_group_id, list_of_best)		
 		
+	
+
+
+# ------ Админ панель ------ #
+@bot.message_handler(commands=['apanel'])
+def apanel(message):
+	fsm.reset_state(message.from_user.id)
+	try:
+		if message.from_user.id in config.admin:
+			bot.send_message(message.from_user.id, "Админ панель", reply_markup=create_inlineKeyboard(language_check()["apanel"]["buttons"], 2))
+	except Exception as e:
+		print(e)
+
+
+# Удаление вопроса
+@bot.callback_query_handler(func=lambda call: True and call.data == "delete_question")
+@log
+def apanel_delete_question(call):
+	try:
+		bot.delete_message(call.from_user.id, call.message.message_id)
+		bot.send_message(call.from_user.id, language_check()["apanel"]["delete_question"]["enter_id"])
+		fsm.set_state(call.from_user.id, "delete_question_id")
+	except Exception as e:
+		print(e)
+
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "delete_question_id")
+@log
+def apanel_accept_delete_question(message):
+	try:
+		text = language_check()
+		try:
+			int(message.text)
+		except:
+			return
+		question = models.Quiz.query.filter_by(id=int(message.text)).first()
+		if question == None:
+			pass
+		else:
+			db.session.delete(question)
+			db.session.commit()
+		bot.send_message(message.from_user.id, "Админ панель", reply_markup=create_inlineKeyboard(language_check()["apanel"]["buttons"], 2))
+	except Exception as e:
+		print(e)
+
+
+
+# Удаление лота
+@bot.callback_query_handler(func=lambda call: True and call.data == "delete_lot")
+@log
+def apanel_delete_question(call):
+	try:
+		bot.delete_message(call.from_user.id, call.message.message_id)
+		lots = models.Auc.query.filter_by(status="inactive").all()
+		text = language_check()
+		if len(lots) == 0:
+			bot.send_message(call.from_user.id, text["apanel"]["end_lot"]["no_lots"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+			return 
+
+		lots_buttons = {}
+		for i in lots:
+			lots_buttons[f"{i.name}({i.cost})"] = f"delete_lot {i.id}"
+
+		bot.send_message(call.from_user.id, text["apanel"]["delete_lot"]["chose_lot"], reply_markup=create_inlineKeyboard(lots_buttons, 2))
+	except Exception as e:
+		print(e)
+
+
+
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "delete_lot")
+@log
+def apanel_delete_question(call):
+	try:
+		lot = models.Auc.query.filter_by(id=call.data.split(" ")[1]).first()
+		text = language_check()
+		db.session.delete(lot)
+		db.session.commit()
+		bot.send_message(call.from_user.id, text["apanel"]["delete_lot"]["deleted"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+	except Exception as e:
+		print(e)
+
 		
+
+
+
+
+
+# Добавление вопроса
+@bot.callback_query_handler(func=lambda call: True and call.data == "add_question")
+@log
+def apanel_add_question(call):
+	tbf.send_form(call.from_user.id, forms.Question(language_check()))
+
+
+@tbf.form_event("add_question", action=["submit"])
+def apanel_accept_question(call,form_data):
+	try:
+		text = language_check()
+		if form_data.media:
+			media_type = form_data.media.media_type	
+			file_id = form_data.media.file_id
+		else:
+			media_type = "text"
+			file_id = "text"
+
+		quiz = models.Quiz(quiz_type=media_type, quiz_media_id=file_id, question=form_data.question, answer=form_data.answer, false=pickle.dumps(form_data.false.split("#")), cost=int(form_data.cost))
+		db.session.add(quiz)
+		db.session.commit()
+		quiz = models.Quiz.query.filter_by(quiz_type=media_type, quiz_media_id=file_id, question=form_data.question, answer=form_data.answer).first()
+		print(quiz)
+		
+		bot.send_message(call.from_user.id, text["apanel"]["add_question"]["question_id"].format(str(quiz.id)))
+		bot.send_message(call.from_user.id, text["apanel"]["add_question"]["added"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+	except Exception as e:
+		print(e)
+
+
+
+
+# Снятие слота
+@bot.callback_query_handler(func=lambda call: True and call.data == "end_lot")
+@log
+def apanel_end_lot(call):
+	try:
+		lots = models.Auc.query.filter_by(status="active").all()
+		bot.delete_message(call.from_user.id, call.message.message_id)
+		text = language_check()
+		if len(lots) == 0:
+			bot.send_message(call.from_user.id, text["apanel"]["end_lot"]["no_lots"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+			return 
+
+		lots_buttons = {}
+		for i in lots:
+			lots_buttons[f"{i.name}({i.cost})"] = f"end_lot {i.id}"
+
+		bot.send_message(call.from_user.id, text["apanel"]["end_lot"]["to_end"], reply_markup=create_inlineKeyboard(lots_buttons, 2))
+	except Exception as e:
+		print(e)
+
+
+
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "end_lot")
+@log
+def apanel_end_lot_accept(call):
+	try:
+		bot.delete_message(call.from_user.id, call.message.message_id)
+		lot = db.session.query(models.Auc, models.ActiveLot).filter(models.Auc.id==call.data.split(" ")[1], models.ActiveLot.lot_id==models.Auc.id).all()
+		print(lot)
+		Players = models.Players.query.filter_by(lot_id=lot[0][0].id)
+		# Проверка есть ли победитель у лота
+		if lot[0][1].winner_id != None:
+			user = models.BotUser.query.filter_by(user_id=lot[0][1].winner_id).first()
+			user.coins = user.coins - lot[0][1].cost
+		else:
+			pass
+		db.session.commit()
+		# выдача победителю и програвшим
+		text = language_check()
+		for i in Players:
+			try:
+				if i.user_id == lot[0][1].winner_id:
+					player = models.BotUser.query.filter_by(user_id=i.user_id).first()
+					bot.send_message(i.user_id, text["bet"]["congratulation"].format(lot[0][0].name, lot[0][1].cost))
+					time.sleep(0.03)
+					bot.send_message(i.user_id, text["bet"]["to_get"])
+					time.sleep(0.03)
+					bot.send_message(config.auc_group_id, text["logs"]["win"].format(f"{player.surname} {player.name}", lot[0][0].name, lot[0][1].cost))
+					
+				else:
+					bot.send_message(i.user_id, text["bet"]["sorry"])
+					time.sleep(0.04)
+
+				db.session.delete(i)
+			except Exception as e:
+				print(e)
+		
+		lot[0][0].status = "inactive"
+		db.session.delete(lot[0][1])
+
+
+		db.session.commit()
+		bot.send_message(call.from_user.id, text["apanel"]["end_lot"]["successfully"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+
+		# Сообщение о новом лоте
+		active_lot = models.ActiveLot.query.first()
+		print(active_lot)
+		if active_lot != None:
+			lot = models.Auc.query.filter_by(id=active_lot.lot_id).first()
+			for i in models.BotUser.query.all():
+				try:
+					bot.send_message(i.user_id, text["apanel"]["chose_lot"]["new_lot"].format(lot.cost), reply_markup=create_markup(text["bet"]["bet"]))
+					time.sleep(0.03)
+					bot.send_photo(i.user_id, lot.pic, lot.disc)
+					time.sleep(0.03)
+				except Exception as e:
+					print(e)
+		else:
+			for i in models.BotUser.query.all():
+				try:
+					bot.send_message(i.user_id, text["bet"]["end"], reply_markup=create_markup(text["quiz"]["start_quiz"]))
+					time.sleep(0.03)
+				except Exception as e:
+					print(e)
+	except Exception as e:
+		print(e)
+
+
+
+	
+
+# Создание слота
+@bot.callback_query_handler(func=lambda call: True and call.data == "create_lot")
+@log
+def apanel_create_lot(call):
+	try:
+		bot.delete_message(call.from_user.id, call.message.message_id)
+		text = language_check()
+		bot.send_message(call.from_user.id, text["apanel"]["create_lot"]["send_pic"])
+		fsm.set_state(call.from_user.id, "create_lot_pic")
+	except Exception as e:
+		print(e)
+
+
+
+@bot.message_handler(content_types=['photo'], func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "create_lot_pic")
+@log
+def apanel_lot_pic(message):
+	try:
+		tmp = fsm.get_state(message.from_user.id)
+		text = language_check()
+		bot.send_message(message.from_user.id, text["apanel"]["create_lot"]["enter_name"])	
+		fsm.set_state(message.from_user.id, "create_lot_name", pic=message.photo[0].file_id)
+	except Exception as e:
+		print(e)
+
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "create_lot_name")
+@log
+def apanel_lot_name(message):
+	tmp = fsm.get_state(message.from_user.id)
+	text = language_check()
+	bot.send_message(message.from_user.id, text["apanel"]["create_lot"]["enter_shortname"])	
+	fsm.set_state(message.from_user.id, "create_lot_shortname", pic=tmp[1]["pic"], name=message.text)
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "create_lot_shortname")
+@log
+def apanel_lot_shortname(message):
+	tmp = fsm.get_state(message.from_user.id)
+	text = language_check()
+	bot.send_message(message.from_user.id, text["apanel"]["create_lot"]["enter_disc"])	
+	fsm.set_state(message.from_user.id, "create_lot_disc", pic=tmp[1]["pic"], name=tmp[1]["name"], shortname=message.text)
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "create_lot_disc")
+@log
+def apanel_lot_disc(message):
+	tmp = fsm.get_state(message.from_user.id)
+	text = language_check()
+	bot.send_message(message.from_user.id, text["apanel"]["create_lot"]["enter_cost"])	
+	fsm.set_state(message.from_user.id, "create_lot_cost", pic=tmp[1]["pic"], name=tmp[1]["name"], shortname=tmp[1]["shortname"], disc=message.text)
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "create_lot_cost")
+@log
+def apanel_lot_cost(message):
+	try:
+		text = language_check()
+		try:
+			int(message.text)
+		except:
+			bot.send_message(message.from_user.id, text["apanel"]["create_lot"]["invalid_value"])
+			return
+		tmp = fsm.get_state(message.from_user.id)
+
+		lot = models.Auc(pic=tmp[1]["pic"], name=tmp[1]["name"], shortname=tmp[1]["shortname"], disc=tmp[1]["disc"], cost=message.text, status="inactive")
+		db.session.add(lot)
+		db.session.commit()
+		fsm.reset_state(message.from_user.id)
+		bot.send_message(message.from_user.id, text["apanel"]["create_lot"]["lot_id"].format(lot.id))
+		bot.send_message(message.from_user.id, text["apanel"]["create_lot"]["created"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+	except Exception as e:
+		print(e)
+
+
+
+# Выпуск лота
+@bot.callback_query_handler(func=lambda call: True and call.data == "send_lot")
+@log
+def apanel_send_lot(call):
+	try:
+		lots = models.Auc.query.all()
+		bot.delete_message(call.from_user.id, call.message.message_id)
+		text = language_check()
+		print(lots)
+		if len(lots) == 0:
+			bot.send_message(call.from_user.id, text["apanel"]["chose_lot"]["no_lots"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+			return 
+
+		lots_buttons = {}
+		for i in lots:
+			if i.status == "inactive":
+				lots_buttons[i.name] = f"chose_lot {i.id}"
+
+		bot.send_message(call.from_user.id, text["apanel"]["chose_lot"]["to_send"], reply_markup=create_inlineKeyboard(lots_buttons, 2))
+	except Exception as e:
+		print(e)
+
+		
+
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "chose_lot")
+@log
+def apanel_accept_send(call):
+	try:
+		bot.delete_message(call.from_user.id, call.message.message_id)
+		text = language_check()
+		lot = models.Auc.query.filter_by(id=call.data.split(" ")[1]).first()
+		lot.status = "active"
+		active_lots = models.ActiveLot.query.all()
+		if len(active_lots) == 0:
+			users = models.BotUser.query.all()
+			for i in users:
+				try:
+					bot.send_message(i.user_id, text["apanel"]["chose_lot"]["message_four"].format(i.coins))
+					time.sleep(0.03)
+					bot.send_message(i.user_id, text["apanel"]["chose_lot"]["new_lot"].format(lot.cost), reply_markup=create_markup(text["bet"]["bet"]))
+					time.sleep(0.03)
+					bot.send_photo(i.user_id, lot.pic, lot.disc)
+					time.sleep(0.03)
+				except Exception as e:
+					print(e, i)
+					continue
+		db.session.add(models.ActiveLot(lot_id=lot.id, cost=lot.cost))
+		db.session.commit()
+		
+		bot.send_message(call.from_user.id, text["apanel"]["chose_lot"]["sended_lot"], reply_markup=create_inlineKeyboard(text["apanel"]["buttons"], 2))
+	except Exception as e:
+		print(e)
+
+
+#-- Цепочки --№
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "view_chain_menu")
+@log
+def apanel_view_chain(call):
+	text = language_check()
+	bot.delete_message(call.from_user.id, call.message.message_id)
+	bot.send_message(call.from_user.id, text["apanel"]["chain"]["menu"], reply_markup=create_inlineKeyboard(text["apanel"]["chain"]["buttons"], 2))
+
+
+# Создание цепочик
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "create_chain")
+@log
+def apanel_create_chain(call):
+	text = language_check()
+	bot.delete_message(call.from_user.id, call.message.message_id)
+	bot.send_message(call.from_user.id, text["apanel"]["chain"]["send_name"])
+	fsm.set_state(call.from_user.id, "enter_chain_name")
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "enter_chain_name")
+@log
+def accept_chain_name(message):
+	text = language_check()
+	bot.send_message(message.from_user.id, text["apanel"]["chain"]["send_message"])
+	fsm.set_state(message.from_user.id, "chain_send_message", name=message.text, args=[])
+
+
+
+
+@bot.message_handler(content_types=['video', 'photo', 'text'], func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "chain_send_message")
+@log
+def accept_chain_message(message):
+	text = language_check()
+	tmp = fsm.get_state(message.from_user.id)
+	if message.text == text["apanel"]["chain"]["stop"]:
+		db.session.add(models.Chain(name=tmp[1]["name"], parts=pickle.dumps(tmp[1]["args"])))
+		db.session.commit()
+		bot.send_message(message.from_user.id, text["apanel"]["chain"]["chain_created"], reply_markup=telebot.types.ReplyKeyboardRemove())
+		fsm.reset_state(message.from_user.id)
+		return
+
+	if message.content_type == "photo":
+		tmp[1]["args"].append(["photo", message.photo[0].file_id, message.caption])
+	elif message.content_type == "text":
+		tmp[1]["args"].append(["text", "text", message.text])
+	elif message.content_type == "video":
+		tmp[1]["args"].append(["video", message.video.file_id, message.caption])
+
+	bot.send_message(message.from_user.id, text["apanel"]["chain"]["press_stp"], reply_markup=create_markup(text["apanel"]["chain"]["stop"]))
+
+
+	fsm.set_state(message.from_user.id, "chain_send_message", name=tmp[1]["name"], args=tmp[1]["args"])
+
+
+# Отправка цепочки
+@bot.callback_query_handler(func=lambda call: True and call.data == "send_chain")
+@log
+def apanel_send_chain(call):
+	chains = models.Chain.query.all()
+	bot.delete_message(call.from_user.id, call.message.message_id)
+	text = language_check()
+	if len(chains) == 0:
+		bot.send_message(call.from_user.id, text["apanel"]["chain"]["no_chains"], reply_markup=create_inlineKeyboard(text["apanel"]["chain"]["buttons"], 2))
+		return 
+
+	chains_buttons = {}
+	for i in chains:
+		chains_buttons[i.name] = f"send_chain {i.id}"
+
+	bot.send_message(call.from_user.id, text["apanel"]["chain"]["to_send"], reply_markup=create_inlineKeyboard(chains_buttons, 2))
+
+
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "send_chain")
+@log
+def apanel_send_chain(call):
+	bot.delete_message(call.from_user.id, call.message.message_id)
+	chain = models.Chain.query.filter_by(id=call.data.split(" ")[1]).first()
+	parts = pickle.loads(chain.parts)
+	for user in models.BotUser.query.all():
+		try:
+			for i in parts:
+				print(i)
+				if i[0] == "photo":
+					bot.send_photo(user.user_id, i[1], caption=i[2])
+				elif i[0] == "video":
+					bot.send_video(user.user_id, i[1], caption=i[2])
+				elif i[0] == "text":
+					bot.send_message(user.user_id, i[2])
+		except Exception as e:
+			print(e)
+	bot.send_message(call.from_user.id, language_check()["apanel"]["chain"]["sended"])
+
+
+
+# Удаление цепочки
+@bot.callback_query_handler(func=lambda call: True and call.data == "del_chain")
+@log
+def apanel_del_chain(call):
+	chains = models.Chain.query.all()
+	bot.delete_message(call.from_user.id, call.message.message_id)
+	text = language_check()
+	if len(chains) == 0:
+		bot.send_message(call.from_user.id, text["apanel"]["chain"]["no_chains"], reply_markup=create_inlineKeyboard(text["apanel"]["chain"]["buttons"], 2))
+		return 
+
+	chains_buttons = {}
+	for i in chains:
+		chains_buttons[i.name] = f"del_chain {i.id}"
+
+	bot.send_message(call.from_user.id, text["apanel"]["chain"]["to_del"], reply_markup=create_inlineKeyboard(chains_buttons, 2))
+
+
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "del_chain")
+@log
+def apanel_del_chain(call):
+	bot.delete_message(call.from_user.id, call.message.message_id)
+	chain = models.Chain.query.filter_by(id=call.data.split(" ")[1]).first()
+	db.session.delete(chain)
+	db.session.commit()
+	bot.send_message(call.from_user.id, language_check()["apanel"]["chain"]["deleted"])
+
+
+
+
+
+
+
+@bot.message_handler(commands=['off_coin'])
+@log
+def off_coin(message):
+	if message.from_user.id in config.mod:
+		fsm.set_state(message.from_user.id, "enter_coins")
+		bot.send_message(message.from_user.id, language_check()["mod"]["enter_coins"])
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "enter_coins")
+@log
+def accept_coins(message):
+	text = language_check()
+	tmp = fsm.get_state(message.from_user.id)
+	bot.send_message(message.from_user.id, text["mod"]["enter_id"])	
+	fsm.set_state(message.from_user.id, "enter_id", coins=message.text)
+
+
+@bot.message_handler(func=lambda message: True and fsm.get_state(message.from_user.id)[0] == "enter_id")
+@log
+def accept_id(message):
+	try:
+		int(message.text)
+	except:
+		return
+	text = language_check()
+	tmp = fsm.get_state(message.from_user.id)
+	user = models.BotUser.query.filter_by(user_id=message.text).first()
+	if user == None:
+		bot.send_message(message.from_user.id, text["mod"]["no_user"])
+	else:
+		bot.send_message(message.from_user.id, text["mod"]["info"].format(message.text, user.name, user.surname), reply_markup=create_inlineKeyboard({"Зачислить":f"give_coins {message.text} {tmp[1]['coins']}"}))
+
+
+@bot.callback_query_handler(func=lambda call: True and call.data.split(" ")[0] == "give_coins")
+@log
+def accept_coins(call):
+	bot.delete_message(call.from_user.id, call.message.message_id)
+	text = language_check()
+	user = models.BotUser.query.filter_by(user_id=int(call.data.split(" ")[1])).first()
+	mod = models.BotUser.query.filter_by(user_id=call.from_user.id).first()
+	bot.send_message(call.from_user.id, text["mod"]["success"].format(call.data.split(" ")[1], call.data.split(" ")[2]))
+	user.coins = user.coins + int(call.data.split(" ")[2])
+	bot.send_message(user.user_id, text["functions"]["get"].format(call.data.split(" ")[2]))
+	db.session.commit()
+	fsm.reset_state(call.from_user.id)
+
+
 		
 		
 		
